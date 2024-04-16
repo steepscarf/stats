86c4b2bd1d6cf9aaa1e0aa1e1d6c1f72071492cc
Artem2008YT <127010939+Artem2008YT@users.noreply.github.com>
2024-04-16 19:39:33 +0800

    Delete src/main/java/de/pdinklag/mcstats directory
    Signed-off-by: Artem2008YT <127010939+Artem2008YT@users.noreply.github.com>

--- src/main/java/de/pdinklag/mcstats/mojang/EmptyResponseException.java
+++
@@ -1,9 +0,0 @@
-package de.pdinklag.mcstats.mojang;
-
-/**
- * Indicates an empty response from the Mojang API.
- */
-public class EmptyResponseException extends RuntimeException {
-    EmptyResponseException() {
-    }
-}
